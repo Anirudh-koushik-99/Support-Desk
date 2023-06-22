08c9b86b16f33f8c734372223017c8859558a91b
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-06-22 20:08:20 +0530

    Adding a Note and Getting the Note to the product

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema({
         type: Boolean,
         required: true,
         default: false
-    }
+    },
 }, 
 {
     timestamps: true,
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,8 +4,16 @@ const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = requir
 
 const { protect } = require ('../middleware/authMiddleware')
 
+//REROUTE INTO NOTE ROUTER
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
 router.route('/:id').get(protect,getTicket).delete(protect,  deleteTicket).put(protect, updateTicket)
+
+
+
+
 
 module.exports = router
